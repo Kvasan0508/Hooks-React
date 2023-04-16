0e4add921702619fc32549e82b5a106e4a0bbfc6
Kvasan0508 <77223405+Kvasan0508@users.noreply.github.com>
2023-04-16 18:02:26 +0530

    2 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { useState, useRef, useEffect } from 'react';
 import './style.css';
 
 export default function App() {
+  const TimerRef = useRef(null);
   const slides = [
     {
       url: 'https://images-eu.ssl-images-amazon.com/images/G/31/home_private_label/moritika/baugwsept/xcm_banners_tile2-kitchen_186x116_in-en._SY116_CB612501062_.jpg',
@@ -17,45 +18,35 @@ export default function App() {
       url: 'https://images-eu.ssl-images-amazon.com/images/G/31/home_private_label/moritika/baugwsept/xcm_banners_tile4-home_186x116_in-en._SY116_CB612501080_.jpg',
     },
   ];
-  const values = [
-    {
-      text: 'hello',
-      text1: 'hi',
-    },
-    {
-      text: 'hello0',
-      text1: 'hi',
-    },
-    {
-      text: 'hello00',
-      text1: 'hi',
-    },
-    {
-      text: 'hello000',
-      text1: 'hi',
-    },
-    {
-      text: 'hello000',
-      text1: 'hi',
-    },
-  ];
-  const [currentIndex, SetCurrentIndex] = useState(0);
+  const [slider, setSlider] = useState(0);
+
+  const prev = () => {
+    const isFirstSlide = slider === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : slider - 1;
+    setSlider(newIndex);
+  };
+  const next = React.useCallback(() => {
+    const isLastSlide = slider === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : slider + 1;
+    setSlider(newIndex);
+  }, [slider, slides.length]);
+
   useEffect(() => {
-    setTimeout(() => {
-      currentIndex === values.length - 1
-        ? SetCurrentIndex(0)
-        : SetCurrentIndex(currentIndex + 1);
-    }, 1000);
-  }, [currentIndex]);
+    if (TimerRef.current) {
+      clearTimeout(TimerRef.current);
+    }
+
+    TimerRef.current = setTimeout(() => {
+      next();
+    }, 2000);
+    return () => clearTimeout(TimerRef.current);
+  }, [next]);
 
   return (
     <div>
-      <p className="slide">
-        {' '}
-        {values[currentIndex].text}
-        <br />
-        {values[currentIndex].text1}
-      </p>
+      <img className="slide" src={slides[slider].url} alt="" />
+      <button onClick={prev}>&lt;</button>
+      <button onClick={next}>&gt;</button>
     </div>
   );
 }
