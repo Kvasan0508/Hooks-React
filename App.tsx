7317b11b6189e75258bd8679b34997c26890a852
Kvasan0508 <77223405+Kvasan0508@users.noreply.github.com>
2023-04-16 12:22:15 +0530

    3 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,33 +1,61 @@
 import * as React from 'react';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './style.css';
 
 export default function App() {
-  const [state, setState] = useState(0);
-  const ref = useRef(0);
+  const slides = [
+    {
+      url: 'https://images-eu.ssl-images-amazon.com/images/G/31/home_private_label/moritika/baugwsept/xcm_banners_tile2-kitchen_186x116_in-en._SY116_CB612501062_.jpg',
+    },
+    {
+      url: 'https://images-eu.ssl-images-amazon.com/images/G/31/img22/AmazonBrands/Bikram/Bikram1/home_186._SY116_CB592083129_.jpg',
+    },
+    {
+      url: 'https://images-eu.ssl-images-amazon.com/images/G/31/img22/AmazonBrands/Bikram/Bikram1/kitchen186._SY116_CB592083129_.jpg',
+    },
+    {
+      url: 'https://images-eu.ssl-images-amazon.com/images/G/31/home_private_label/moritika/baugwsept/xcm_banners_tile4-home_186x116_in-en._SY116_CB612501080_.jpg',
+    },
+  ];
+  const values = [
+    {
+      text: 'hello',
+      text1: 'hi',
+    },
+    {
+      text: 'hello0',
+      text1: 'hi',
+    },
+    {
+      text: 'hello00',
+      text1: 'hi',
+    },
+    {
+      text: 'hello000',
+      text1: 'hi',
+    },
+    {
+      text: 'hello000',
+      text1: 'hi',
+    },
+  ];
+  const [currentIndex, SetCurrentIndex] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      currentIndex === values.length - 1
+        ? SetCurrentIndex(0)
+        : SetCurrentIndex(currentIndex + 1);
+    }, 1000);
+  }, [currentIndex]);
 
-  const handleState = () => {
-    setState(state + 1);
-  };
-  const handleStatedec = () => {
-    setState(state - 1);
-  };
-  const handleRef = () => {
-    console.log('ref');
-    ref.current++;
-  };
   return (
     <div>
-      <p>usestate</p> {state} <br />
-      <button onClick={handleState}> Click Me increment</button>
-      <br />
-      <button onClick={handleStatedec}> Click Me decrement</button>
-      <br />
-      <p>useref</p>
-      {ref.current}
-      <br />
-      <button onClick={handleRef}> Click Me increment</button>
-      <br />
+      <p className="slide">
+        {' '}
+        {values[currentIndex].text}
+        <br />
+        {values[currentIndex].text1}
+      </p>
     </div>
   );
 }
---
+++ backup.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { useState, useRef, useEffect } from 'react';
+import './style.css';
+
+export default function App() {
+  const [state, setState] = useState(0);
+  const ref = useRef(0);
+
+  const handleState = () => {
+    setState(state + 1);
+  };
+  const handleStatedec = () => {
+    setState(state - 1);
+  };
+  const handleRef = () => {
+    console.log('ref');
+    ref.current++;
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      setState(state + 1);
+    }, 1000);
+  }, [state]);
+  return (
+    <div>
+      <p>usestate</p> {state} <br />
+      <button onClick={handleState}> Click Me increment</button>
+      <br />
+      <button onClick={handleStatedec}> Click Me decrement</button>
+      <br />
+      <p>useref</p>
+      {ref.current}
+      <br />
+      <button onClick={handleRef}> Click Me increment</button>
+      <br />
+    </div>
+  );
+}
